574fe9f5050875e9abdcda7f93d0b3c0133a2c99
Volodymyr Zhdanov <wight554@gmail.com>
2019-09-23 00:02:45 +0300

    install.sh: setup self compiled alacritty package
    * also set git configs

--- install.sh
+++ install.sh
@@ -11,18 +11,26 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay
 
+# Set git configs
+git config --global user.email "wight554@gmail.com"
+git config --global user.name "Volodymyr Zhdanov"
+
 # Chromium packages
 yay -S chromium ttf-liberation --noconfirm
 
 # Minimal packages setup
-# alacritty is built from master
-yay -S fish starship-bin alacritty-terminfo compton --noconfirm
+yay -S fish starship-bin alacritty compton --noconfirm
 
 # UI customizations
 yay -S plata-theme papirus-icon-theme --noconfirm
 
 # Fonts
 yay -S ttf-fira-code ttf-dejavu noto-fonts noto-fonts-cjk noto-fonts-emoji --noconfirm
+
+# Replace alacritty with custom package
+wget https://github.com/wight554/alacritty/releases/download/untagged-8f3f74a594ddea869359/Alacritty-16ba9da-ubuntu_18_04_amd64.tar.gz -O alacritty.tar.gz
+tar -xvf alacritty.tar.gz
+sudo install -D -m755 alacritty /usr/bin/alacritty
 
 # Custom ls colors
 wget https://raw.github.com/trapd00r/LS_COLORS/master/LS_COLORS -O $HOME/.dircolors
